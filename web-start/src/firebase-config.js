29a1d563b4e5c57a962d7d1730d0338d30e57eb2
Ziwen Zeng <ziwenzeng@ziwens-mbp-2.myfiosgateway.com>
2022-03-25 16:58:17 -0400

    Setup finished

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -6,9 +6,27 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
+
+
+// const config = {
+//   apiKey: "AIzaSyAag0XZs5eHDHoAduOjie2NtLRTmK10mA8",
+//   authDomain: "friendlychat-c46f6.firebaseapp.com",
+//   projectId: "friendlychat-c46f6",
+//   storageBucket: "friendlychat-c46f6.appspot.com",
+//   messagingSenderId: "911089474345",
+//   appId: "1:911089474345:web:8ab7500f4d78c21387f538"
+// };
+
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
-};
+  apiKey: "AIzaSyAag0XZs5eHDHoAduOjie2NtLRTmK10mA8",
+  authDomain: "friendlychat-c46f6.firebaseapp.com",
+  databaseURL: "https://friendlychat-c46f6-default-rtdb.firebaseio.com",
+  projectId: "friendlychat-c46f6",
+  storageBucket: "friendlychat-c46f6.appspot.com",
+  messagingSenderId: "911089474345",
+  appId: "1:911089474345:web:8ab7500f4d78c21387f538"
+};// Miss measurementId????
+
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
